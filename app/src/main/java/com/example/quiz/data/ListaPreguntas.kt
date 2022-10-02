cdf1f349f00a5523999de369495872bc78fb9074
ANDRESCZ <andres.cz.chavez@gmail.com>
2022-10-02 13:11:46 -0500

    Actualización de preguntas

--- app/src/main/java/com/example/quiz/data/ListaPreguntas.kt
+++ app/src/main/java/com/example/quiz/data/ListaPreguntas.kt
@@ -17,39 +17,41 @@ object ListaPreguntas {
                 ), "https://upload.wikimedia.org/wikipedia/commons/2/24/Bogot%C3%A1_Colpatria_Night.jpg",
                     "Oficialmente Bogotá, Distrito Capital (antiguamente, Santafé de Bogotá)"
             ),
-                Pregunta("¿Entre que siglos fue construida gran la muralla china ?",
+                Pregunta("¿Quién es el autor de la frase 'Pienso, luego existo'?",
                     mutableListOf(
-                        Respuesta("1982", true),
-                        Respuesta("1898", false),
-                        Respuesta("1931", false)
+                        Respuesta("Descartes", true),
+                        Respuesta("Platón", false),
+                        Respuesta("Sócrates", false)
                     ),
-                    descripcionRespuesta = "Porque es una de las capilates más grandes" // en caso de no tener una imagen hay que especificar el nombre de la propiedad
+                    // en caso de no tener una imagen hay que especificar el nombre de la propiedad
+                    descripcionRespuesta = "'Je pense, donc je suis' es la frase original, escrita en francés, del filósofo René Descartes (1596-1650). Esta frase resume el pensamiento y el método de Descartes, para quien todo se inicia con la duda"
                 ),
-                Pregunta("¿3 -> ¿Cuál es la forma de gobierno de Colombia?",
+                Pregunta("¿Cuántos litros de sangre tiene una persona adulta?",
                     mutableListOf(
-                        Respuesta("República presidencialista", true),
-                        Respuesta("Congreso", false),
-                        Respuesta("Ninguno", false)
+                        Respuesta("Tiene entre 2 y 4 litros", false),
+                        Respuesta("Tiene entre 4 y 6 litros", true),
+                        Respuesta("Tiene 10 litros", false)
                     ),
-                    descripcionRespuesta = "Porque es una de las capilates más grandes"
+                    descripcionRespuesta = "La cantidad de sangre varía de persona a persona. El volumen de sangre representa del 7 al 8% del peso corporal. De esta forma, en una persona adulta que pesa entre 50 y 80 kilogramos, puede haber entre 4 y 6 litros de sangre."
 
                 ),
-                Pregunta("4 ¿Cómo se llama el aeropuerto de Bogotá?",
+                Pregunta("¿Cómo se llama el aeropuerto de Bogotá?",
                     mutableListOf(
-                        Respuesta("El Dorado", false),
+                        Respuesta("El Dorado", true),
                         Respuesta("Aguas Claras", false),
-                        Respuesta("Baracoa", true)
+                        Respuesta("Baracoa", false)
                     ),
-                    descripcionRespuesta = "Porque es una de las capilates más grandes"
+                    descripcionRespuesta = "El Dorado Luis Carlos Galán Sarmiento más conocido como Aeropuerto de El Dorado, es el principal aeropuerto de Colombia. Se encuentra localizado dentro de Bogotá"
 
                 ),
-                Pregunta("¿5 Quién fundó Santa Marta, capital del departamento Magdalena en Colombia? ",
+                Pregunta("¿Cuál es el país más grande y el más pequeño del mundo? ",
                     mutableListOf(
-                        Respuesta("Rodrigo de Bastidas", false),
-                        Respuesta("Fransisco", true),
-                        Respuesta("Camilo ", false)
+                        Respuesta("Rusia y Vaticano", true),
+                        Respuesta("China y Nauru", false),
+                        Respuesta("India y San Marino ", false)
                     ),
-                    descripcionRespuesta = "Porque es una de las capilates más grandes"
+                    "https://www.infobae.com/new-resizer/1NstrfwipAGEN_wSRRhwcHApfVk=/1200x628/filters:format(webp):quality(85)//cloudfront-us-east-1.images.arcpublishing.com/infobae/ULKTOARPZBA4HNYABMXB22UIQ4.jpg",
+                    descripcionRespuesta = "Rusia es el país mas grande del mundo con un área de 17 millones de km2, mientras el Vaticano tiene apenas 0,44 km2."
                 )
             )
         )
